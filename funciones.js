04853e31ec966f4171a2588f8abe201ce1faab2b
frderickk <federicofarias@outlook.com>
2022-05-27 20:59:21 -0300

    Intento de escritura de JSON

--- funciones.js
+++ funciones.js
@@ -1,9 +1,42 @@
-function main() {
+
+function main() {  
+   // document.querySelector('#cargar').addEventListener('click', guardarDatosPlantas);
     recargar();
 }
+
 function recargar() {
     let refresh = document.getElementById('ingresar_planta')
     refresh.addEventListener('click', _ => {
         location.reload();
     })
 }
+
+/*var datosPlantas = []
+
+function agregarDatosPlantas(nombre, familia, fila, columna, edad) {
+    
+    var nuevaPlanta = {
+        nombre: nombre,
+        familia: familia,
+        fila: fila,
+        columna: columna,
+        edad: edad
+    };
+
+    console.log(nuevaPlanta); 
+    datosPlantas.push(nuevaPlanta);
+}
+
+function obtenerListaPlantas() {
+    return datosPlantas;
+}
+
+function guardarDatosPlantas() {
+    var guardarPlantaNombre = document.querySelector('#nombre').value,
+        guardarPlantaFamilia = document.querySelector('#familia').value,
+        guardarPlantaFila = document.querySelector('#fila').value;
+        guardarPlantaColumna = document.querySelector('#columna').value;
+        guardarPlantaEdad = document.querySelector('#edad').value;
+    
+    agregarDatosPlantas(guardarPlantaNombre, guardarPlantaFamilia, guardarPlantaFila, guardarPlantaColumna, guardarPlantaEdad);
+}*/
