77be2c682f25e621ce2c676793b31a587432f408
frderickk <federicofarias@outlook.com>
2022-06-10 22:17:58 -0300

    v2

--- funciones.js
+++ funciones.js
@@ -73,6 +73,21 @@ function enviarRespuesta(response , elementoId){
     document.getElementById(elementoId).append(text);
 }
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("carruselImagenes");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
+
 /*var datosPlantas = []
 
 function agregarDatosPlantas(nombre, familia, fila, columna, edad) {
